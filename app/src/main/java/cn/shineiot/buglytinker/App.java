24ff8a11ab3ed730469793475dc16a8868445fe3
wangshuai <bjwangs@163.com>
2019-07-26 16:41:30 +0800

    '..'

---
+++ app/src/main/java/cn/shineiot/buglytinker/App.java
@@ -0,0 +1,25 @@
+package cn.shineiot.buglytinker;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+
+public class App extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Bugly.init(getApplicationContext(),"650763bceb",true);
+
+        Beta.installTinker();
+    }
+}
